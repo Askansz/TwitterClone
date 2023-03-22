530c577735a03bac7c94ffe27eba6713813d8c29
Askansz <122436444+Askansz@users.noreply.github.com>
2023-03-22 14:55:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-// Store tweets in an array
-let tweets = [];
+// Selecting elements
+const form = document.querySelector('#message-form');
+const messageList = document.querySelector('#message-list');
+const messageInput = document.querySelector('#message-input');
 
-// Get the tweet form and input element
-const tweetForm = document.getElementById('tweet-form');
-const tweetInput = document.getElementById('tweet-input');
-
-// Get the tweet list element
-const tweetList = document.getElementById('tweet-list').querySelector('ul');
-
-// Add event listener to tweet form
-tweetForm.addEventListener('submit', (e) => {
-    e.preventDefault(); // Prevent default form submission behavior
-    const tweetText = tweetInput.value; // Get the text of the new tweet
-    const tweet = { text: tweetText }; // Create a new tweet object
-    tweets.push(tweet); // Add the tweet object to the tweets array
-    tweetInput.value = ''; // Clear the tweet input field
-    console.log(tweets); // Log the tweets array to the console
-    renderTweets(); // Render the updated list of tweets
-});
-
-// Function to render the list of tweets
-function renderTweets() {
-    tweetList.innerHTML = ''; // Clear the existing list
-    for (let i = 0; i < tweets.length; i++) {
-        const tweet = tweets[i];
-        const li = document.createElement('li'); // Create a new list item element
-        li.innerText = tweet.text; // Set the text content of the list item to the tweet text
-        tweetList.appendChild(li); // Append the list item to the tweet list
-    }
+// Function to create a new message
+function createMessage(text) {
+  const message = document.createElement('div');
+  message.classList.add('message');
+  const messageText = document.createElement('p');
+  messageText.textContent = text;
+  const messageDate = document.createElement('span');
+  messageDate.textContent = new Date().toLocaleString();
+  message.appendChild(messageText);
+  message.appendChild(messageDate);
+  return message;
 }
+
+// Function to handle form submission
+function handleSubmit(event) {
+  event.preventDefault();
+  const messageText = messageInput.value.trim();
+  if (messageText !== '') {
+    const message = createMessage(messageText);
+    messageList.appendChild(message);
+    messageInput.value = '';
+  }
+}
+
+// Adding event listener to the form
+form.addEventListener('submit', handleSubmit);
