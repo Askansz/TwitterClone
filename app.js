3b8f7a8ba62f86fa4c9f3403c643d39e1a63f0f3
Askansz <122436444+Askansz@users.noreply.github.com>
2023-03-22 13:07:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+// Store tweets in an array
+let tweets = [];
 
+// Get the tweet form and input element
+const tweetForm = document.getElementById('tweet-form');
+const tweetInput = document.getElementById('tweet-input');
+
+// Add event listener to tweet form
+tweetForm.addEventListener('submit', (e) => {
+    e.preventDefault(); // Prevent default form submission behavior
+    const tweetText = tweetInput.value; // Get the text of the new tweet
+    const tweet = { text: tweetText }; // Create a new tweet object
+    tweets.push(tweet); // Add the tweet object to the tweets array
+    tweetInput.value = ''; // Clear the tweet input field
+    console.log(tweets); // Log the tweets array to the console
+});
