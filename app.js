5ab26c4eb69af3adc15e4dd29b04dfb739e60b2c
Askansz <122436444+Askansz@users.noreply.github.com>
2023-03-22 13:15:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ let tweets = [];
 const tweetForm = document.getElementById('tweet-form');
 const tweetInput = document.getElementById('tweet-input');
 
+// Get the tweet list element
+const tweetList = document.getElementById('tweet-list').querySelector('ul');
+
 // Add event listener to tweet form
 tweetForm.addEventListener('submit', (e) => {
     e.preventDefault(); // Prevent default form submission behavior
@@ -13,4 +16,16 @@ tweetForm.addEventListener('submit', (e) => {
     tweets.push(tweet); // Add the tweet object to the tweets array
     tweetInput.value = ''; // Clear the tweet input field
     console.log(tweets); // Log the tweets array to the console
+    renderTweets(); // Render the updated list of tweets
 });
+
+// Function to render the list of tweets
+function renderTweets() {
+    tweetList.innerHTML = ''; // Clear the existing list
+    for (let i = 0; i < tweets.length; i++) {
+        const tweet = tweets[i];
+        const li = document.createElement('li'); // Create a new list item element
+        li.innerText = tweet.text; // Set the text content of the list item to the tweet text
+        tweetList.appendChild(li); // Append the list item to the tweet list
+    }
+}
